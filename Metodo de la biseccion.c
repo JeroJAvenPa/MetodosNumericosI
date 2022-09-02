f3f6afdce4fc298793f1bf44b09e36a4f009e0a5
Jerojojojo <jeronimojahir@hotmail.com>
2022-09-02 00:32:23 -0500

    no message

--- Metodo de la biseccion.c
+++ Metodo de la biseccion.c
@@ -13,21 +13,22 @@ double fx(double x){
 }
 
 int main(){
-	double a, b, p;
+	double a, b, p, error, s1, s2;
 	int i=0;
 	printf("\tDame el intervalo de solución \n");
 	printf("a: "); scanf("%lf", &a);
 	printf("b: "); scanf("%lf", &b);
 	
 	system("cls");
-	
-	printf("i\ta\tb\tf(a)\t\tf(b)\t\tp\t\t\tf(p)\n");
+	printf("i\ta\tb\tf(a)\t\tf(b)\t\tp\t\t\tf(p)\t\terror\n");
 	
 	do{
 		p=(a+b)/2;
-		
-		printf("%d\t%.3f\t%.3f\t%.6f\t%f\t%.17f\t%.10f\n", i+1, a, b, fx(a), fx(b), p, fx(p) );
-		if(fx(a)*fx(p)<0)
+		s2=s1;
+		s1=fx(p);
+		error=fabs((s1-s2)/s1);
+		printf("%d\t%.3f\t%.3f\t%.6f\t%f\t%.17f\t%.10f\t%.6f\n", i+1, a, b, fx(a), fx(b), p, s1, error );
+		if(fx(a)*s1<0)
 			b=p;
 		else a=p;
 		i++;
